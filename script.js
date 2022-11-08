93b4a129f0cc98708a8bc2b644297fa55facc216
Lucas Alexandre Neves <lucaslhneves@gmail.com>
2022-11-07 22:57:39 -0300

    Configured the colorClickEvent function so that it recognizes the color chosen on click.

--- script.js
+++ script.js
@@ -4,6 +4,10 @@ document.querySelectorAll('.colorArea .color').forEach(item => {
     item.addEventListener('click', colorClickEvent)
 })
 
-function colorClickEvent(){
-    
+function colorClickEvent(e){
+    let color = e.target.getAttribute('data-color')
+    currentColor = color
+
+    document.querySelector('.color.active').classList.remove('active')
+    e.target.classList.add('active')
 }
