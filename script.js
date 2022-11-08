e7ff022855c04d9f2bb729c47777dfcd36b3c742
Lucas Alexandre Neves <lucaslhneves@gmail.com>
2022-11-07 23:31:13 -0300

    Configuration of mouse Down, Move and Up functions.

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 // Initial Data
 let currentColor = 'black'
+let canDraw = false
+let mouseX = 0
+let mouseY = 0
 
 let screen = document.querySelector('#tela')
 let ctx = screen.getContext('2d')
@@ -30,13 +33,34 @@ function colorClickEvent(e){
 }
 
 function mouseDownEvent(){
-
+    canDraw = true
+    mouseX = e.pageX - screen.offsetLeft
+    mouseY = e.pageY - screen.offsetTop
 }
 
-function mouseMoveEvent(){
-
+function mouseMoveEvent(e){
+    if(canDraw){
+       Draw(e.pageX, e.pageY)
+    }
 }
 
 function mouseUpEvent(){
-    
+    canDraw = false
+}
+
+function Draw(x, y){
+    let pointX = x - screen.offsetLeft
+    let pointY = y - screenTop
+
+    ctx.beginPath()
+    ctx.lineWidth = 5
+    ctx.lineJoin = "round"
+    ctx.moveTo(mouseX, mouseY)
+    ctx.lineTo(pointX, pointY)
+    ctx.closePath()
+    ctx.strokeStyle = currentColor
+    ctx.stroke()
+
+    mouseX = pointX
+    mouseY = pointY
 }
