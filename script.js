76c4c437f426a89c1c5b91a3536b189c0b678676
Lucas Alexandre Neves <lucaslhneves@gmail.com>
2022-11-07 22:52:35 -0300

    The first step is to be able to store which color is selected and add the click event to select a color so I created the colorclickEvent() function.

---
+++ script.js
@@ -0,0 +1,9 @@
+let currentColor = 'black'
+
+document.querySelectorAll('.colorArea .color').forEach(item => {
+    item.addEventListener('click', colorClickEvent)
+})
+
+function colorClickEvent(){
+    
+}
