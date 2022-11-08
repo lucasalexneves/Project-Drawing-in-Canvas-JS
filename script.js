83e90069a9b021002b6bf76d732390bc6196120d
Lucas Alexandre Neves <lucaslhneves@gmail.com>
2022-11-07 23:09:54 -0300

    To draw in Canva I have to monitor mouse click, movement and release. So I created 3 functions for this.

--- script.js
+++ script.js
@@ -8,6 +8,17 @@ let ctx = screen.getContext('2d')
 document.querySelectorAll('.colorArea .color').forEach(item => {
     item.addEventListener('click', colorClickEvent)
 })
+screen.addEventListener('mousedown', mouseDownEvent)
+screen.addEventListener('mousemove', mouseMoveEvent)
+screen.addEventListener('mouseup', mouseUpEvent)
+
+
+/*
+Step by step to draw on canvas:
+- When mouse click DOWN, activate drawing mode.
+- When the mouse MOVES, if drawing mode is on, draw.
+- When mouse click RAISE, deactivate drawing mode.
+*/
 
 // Functions
 function colorClickEvent(e){
@@ -16,4 +27,16 @@ function colorClickEvent(e){
 
     document.querySelector('.color.active').classList.remove('active')
     e.target.classList.add('active')
+}
+
+function mouseDownEvent(){
+
+}
+
+function mouseMoveEvent(){
+
+}
+
+function mouseUpEvent(){
+    
 }
